3fbbb063028ee354526be6579cef1fc4561001a3
hauretax <huggotrico@protonmail.ch>
2023-02-07 19:08:26 +0100

    date showing on month

--- look-at-me/src/calendar/Calendar.js
+++ look-at-me/src/calendar/Calendar.js
@@ -20,15 +20,26 @@ function getMonthDays(year, month) {
     const numberDays = daysInMonth(year, month)
     const tab = [];
     for (let i = 1; i <= numberDays; i++) {
-        tab.push({nb : i, class: 'calendar-table__event'})
+        const datetofind = `${i}-${month}-${year}`
+        tab.push(tmpEvents.find(e => e.dateText === datetofind) ? { nb: i, class: 'calendar-table__event' } : { nb: i, class: '' })
     }
     for (let i = 1; i < dayToMoov; i++) {
-        tab.unshift({nb: ' ', class: 'noHover'})
+        tab.unshift({ nb: ' ', class: 'noHover' })
     }
     return tab
 }
 
-const tmpEvents = [{ title: 'Town hall meeting', dateStart: 1693200000, dateEnd: 1693286400000 },
+const tmpEvents = [{ title: 'Town hall meeting', dateStart: 1640486400000, dateEnd: 1640569600000 },
+{ title: 'Community gathering', dateStart: 1648684800000, dateEnd: 1648751200000 },
+{ title: 'City council meeting', dateStart: 1655036800000, dateEnd: 1655081600000 },
+{ title: 'Public forum', dateStart: 1661604800000, dateEnd: 1661659200000 },
+{ title: 'Neighborhood meeting', dateStart: 1666022400000, dateEnd: 1666080000000 },
+{ title: 'Town hall discussion', dateStart: 1676614400000, dateEnd: 1676694400000 },
+{ title: 'Municipal meeting', dateStart: 1685772800000, dateEnd: 1685852400000 },
+{ title: 'Local government meeting', dateStart: 1693758400000, dateEnd: 1693835200000 },
+{ title: 'Community council meeting', dateStart: 1703305600000, dateEnd: 1703386800000 },
+{ title: 'City hall meeting', dateStart: 1711678400000, dateEnd: 1711758400000 },
+{ title: 'Town hall meeting', dateStart: 1693200000, dateEnd: 1693286400000 },
 { title: 'Community gathering', dateStart: 1704608000000, dateEnd: 1704780800000 },
 { title: 'City council meeting', dateStart: 1717440000000, dateEnd: 1717484800000 },
 { title: 'Public forum', dateStart: 1730240000000, dateEnd: 173030400000 },
@@ -37,11 +48,37 @@ const tmpEvents = [{ title: 'Town hall meeting', dateStart: 1693200000, dateEnd:
 { title: 'Municipal meeting', dateStart: 1767232000000, dateEnd: 176731600000 },
 { title: 'Local government meeting', dateStart: 1781712000000, dateEnd: 178176800000 },
 { title: 'Community council meeting', dateStart: 179450400000, dateEnd: 179454800000 },
-{ title: 'City hall meeting', dateStart: 1806912000000, dateEnd: 1806956800000 }
-]
-function getEvent() {
+{ title: 'City hall meeting', dateStart: 1806912000000, dateEnd: 1806956800000 },
+{ title: 'Town hall meeting', dateStart: 1819321600000, dateEnd: 1819398400000 },
+{ title: 'Community gathering', dateStart: 1831724800000, dateEnd: 1831804800000 },
+{ title: 'City council meeting', dateStart: 1845472000000, dateEnd: 1845516800000 },
+{ title: 'Public forum', dateStart: 1858272000000, dateEnd: 185833600000 },
+{ title: 'Neighborhood meeting', dateStart: 1867872000000, dateEnd: 186793600000 },
+{ title: 'Town hall discussion', dateStart: 1882086400000, dateEnd: 1882166400000 },
+{ title: 'Municipal meeting', dateStart: 1894982400000, dateEnd: 189504800000 },
+{ title: 'Local government meeting', dateStart: 1909462400000, dateEnd: 190952800000 },
+{ title: 'Community council meeting', dateStart: 192195200000, dateEnd: 192199600000 },
+{ title: 'City hall meeting', dateStart: 1934361600000, dateEnd: 1934435200000 },
+{ title: 'Town hall meeting', dateStart: 1946860800000, dateEnd: 1946934400000 },
+{ title: 'Community gathering', dateStart: 1959260000000, dateEnd: 1959333600000 },
+{ title: 'City council meeting', dateStart: 1972824000000, dateEnd: 1972868800000 },
+{ title: 'Public forum', dateStart: 1985624000000, dateEnd: 1985680800000 },
+{ title: 'Neighborhood meeting', dateStart: 1995224000000, dateEnd: 199528800000 },
+{ title: 'Town hall discussion', dateStart: 2009527200000, dateEnd: 2009607200000 },
+{ title: 'Municipal meeting', dateStart: 2022531200000, dateEnd: 202259200000 },
+];
 
-    return tmpEvents
+
+function getEvent(year, month) {
+    let events = [];
+    tmpEvents.forEach(function (el) {
+
+        const elDate = new Date(el.dateStart)
+        el.dateText = `${elDate.getDate()}-${elDate.getMonth()}-${elDate.getFullYear()}`
+        if (elDate.getMonth() === month && elDate.getFullYear() === year)
+            events.push(el)
+    })
+    return events
 }
 
 
@@ -54,9 +91,11 @@ export default function Calendar() {
     const [month, setMonth] = useState(date.getMonth());
     const [year, setYear] = useState(date.getFullYear());
     const [calendTab, setCalend] = useState(getMonthDays(year, month + 1));
+    console.log(month)
+    const [events, setEvents] = useState(getEvent(year, month + 1));
 
-    const [events, setEvents] = useState(getEvent());
 
+    console.log(events)
     // console.log(month, getMonthDays(2023, 2))
 
     function moovMonth(value) {
@@ -75,6 +114,8 @@ export default function Calendar() {
         setYear(tmpYear);
         setMonth(tmpMonth);
         setCalend(getMonthDays(tmpYear, tmpMonth + 1))
+        console.log(month)
+        setEvents(getEvent(tmpYear, tmpMonth + 1))
     }
 
     return (
