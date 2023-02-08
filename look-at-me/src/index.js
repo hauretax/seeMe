f60094c5f67944bdf3dd32be887ffe53e75a1aa7
Huggo TRICOTTET <hutricot@e1r12p13.clusters.42paris.fr>
2023-02-08 16:33:22 +0100

    add way to puh direct calendar

--- look-at-me/src/index.js
+++ look-at-me/src/index.js
@@ -4,11 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Calendar from './calendar/Calendar';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route exact path='/' element={<App />} />
+      <Route exact path='/calendar' element={< Calendar />} />
+      <Route exact path='/agenda' element={< Calendar />} />
+    </Routes>
+  </Router>,
 );
 
 // If you want to start measuring performance in your app, pass a function
